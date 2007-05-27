9fe16933b6d15f94d441d266c66ecec60259ee32
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-05-27 07:26:34 +0000

    Added basic highscore framework
    git-svn-id: svn://localhost/home/nikhil/repository/tetrablocks/trunk@469 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ src/highscore.h
@@ -0,0 +1,71 @@
+/******************************
+ * highscore.h - manages high scores
+ * Independent framework. Can be
+ * used in other projects.
+ *
+ * Created - May 26, 2007
+ *
+ * (C) Nikhil Marathe <nsm.nikhil@gmail.com>
+ * Licensed under the GNU GPL
+ */
+
+#ifndef NMUTILS_HIGHSCORE_H
+#define NMUTILS_HIGHSCORE_H
+
+#include<fstream>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+namespace NMUtils {
+    /*
+     * A class to wrap a score, consisting of score and name
+     *
+     * @version 1.0
+     * @author Nikhil Marathe <nsm.nikhil@gmail.com>
+     */
+    class Score {
+    private:
+        enum {DELIM=' '};
+        int score;
+        string name;
+    public:
+        /**
+         * Create a new Score object with score sc and name nam
+         *
+         * @param sc The score
+         * @param nam Name
+         */
+        Score(int sc, string nam) {
+            score = sc;
+            name = nam;
+        };
+
+        int getScore() { return score; };
+        string getName() { return name; };
+        
+        bool operator==(const Score &other) const {
+            return score == other.score;
+        };
+
+        bool operator!=(const Score &other) const {
+            return !(*this == other);
+        };
+
+		friend std::ostream& operator<< (std::ostream& os, const Score &s ) {
+			return os<<s.score<<(char)DELIM<<s.name<<endl;
+		};
+    };
+
+    class Highscore {
+    private:
+        fstream  * hs_file;
+
+    public:
+        Highscore(const char *);
+        bool bad();
+    };
+};
+
+#endif
