1195203e8bcb93114e8e4784445c53457f130951
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-05-28 15:40:04 +0000

    Added menu.h with basic code
    git-svn-id: svn://localhost/home/nikhil/repository/tetrablocks/trunk@473 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

---
+++ src/menu.h
@@ -0,0 +1,16 @@
+/******************************
+ * menu.h - Menu class
+ * Requires SDL_ttf
+ * Created - May 27, 2007
+ *
+ * (C) Nikhil Marathe <nsm.nikhil@gmail.com>
+ * Licensed under the GNU GPL
+ */
+
+#ifndef NMUTILS_MENU_H
+#define NMUTILS_MENU_H
+
+#include<SDL/SDL_ttf>
+
+namespace NMUtils {
+
