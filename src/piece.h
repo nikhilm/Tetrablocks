06777dbd92c12f047b90f720b7c654a2b3b81616
(no author) <(no author)@7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6>
2007-06-20 11:14:59 +0000

    Added setX and setY to Piece
    git-svn-id: svn://localhost/home/nikhil/repository/tetrablocks/trunk@550 7e96f2c5-ff98-4bf4-9c58-b8ea98d1b7b6

--- src/piece.h
+++ src/piece.h
@@ -48,6 +48,8 @@ namespace TetraBlocks {
 
             int getX() { return x; };
             int getY() { return y; };
+            void setX(int X) { x = X; };
+            void setY(int Y) { y = Y; };
 
             bool bottomCollision();
 
